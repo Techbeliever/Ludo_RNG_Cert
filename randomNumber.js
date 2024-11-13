296a040f591f413e49ed8253f6b8198fe312e0cd
gk-adda <91180087+gk-adda@users.noreply.github.com>
2024-11-13 14:26:19 +0530

    Update randomNumber.js

--- randomNumber.js
+++ randomNumber.js
@@ -1,3 +1,4 @@
+const xorshift = require('xorshift');
 /** get random number between 1 to 6 **/
 const getRollDiceNumber = async (min=1, max=6) => {
     return await Math.floor(Math.random()*(max-min+1)+min);
@@ -7,3 +8,10 @@ const getRollDiceNumber = async (min=1, max=6) => {
 const getRollDiceNumberLowerThanSix = async (min=1, max=5) => {
     return await Math.floor(Math.random()*(max-min+1)+min);
 };
+
+/** function to generate random dice number */
+function uniformint(a, b) {
+    return Math.floor(a + xorshift.random() * (b - a));
+}
+
+uniformint(1, 7);
