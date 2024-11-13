5cb4e870520a1d15fbf29cfe5d159742a286e1b4
Rahul <unity.rahul@khiladiadda.comunity.rahul@khiladiadda.comunity.rahul@khiladiadda.com>
2024-11-13 15:30:27 +0530

    Random number

--- randomNumber.js
+++ randomNumber.js
@@ -1,13 +1,5 @@
 const xorshift = require('xorshift');
-/** get random number between 1 to 6 **/
-const getRollDiceNumber = async (min=1, max=6) => {
-    return await Math.floor(Math.random()*(max-min+1)+min);
-};
 
-/** get random number between 1 to 5 **/
-const getRollDiceNumberLowerThanSix = async (min=1, max=5) => {
-    return await Math.floor(Math.random()*(max-min+1)+min);
-};
 
 /** function to generate random dice number */
 function uniformint(a, b) {
